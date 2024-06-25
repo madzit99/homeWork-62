f07e61c26e36db6a1093ae30398f93cfbe9c96af
Aliev Madjit <madzitaliev51@gmail.com>
2024-06-25 17:01:10 +0600

    Сделал контейнеры Fighters, Rating, Ffight и вывел их на страницу

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { Route, Routes } from "react-router-dom";
 import Footer from "./Components /Footer/Footer";
 import Toolbar from "./Components /Toolbar/Toolbar";
 import Home from "./Containers/Home";
+import Fighters from "./Containers/Fighters";
+import Rating from "./Containers/Rating";
+import Fight from "./Containers/Fight";
 
 const App = () => {
   return (
@@ -14,13 +17,13 @@ const App = () => {
           <Route path="/" element={<Home />} />
         </Routes>
         <Routes>
-          <Route path="/fighters" element={<Home />} />
+          <Route path="/fighters" element={<Fighters />} />
         </Routes>
         <Routes>
-          <Route path="/rating" element={<Home />} />
+          <Route path="/rating" element={<Rating />} />
         </Routes>
         <Routes>
-          <Route path="/fight" element={<Home />} />
+          <Route path="/fight" element={<Fight />} />
         </Routes>
       </main>
       <footer>
