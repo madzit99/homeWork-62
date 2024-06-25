911cfc5f8fe7411bf01a080e3d10a336b763aa0b
Aliev Madjit <madzitaliev51@gmail.com>
2024-06-25 15:56:08 +0600

    Настроил Роуты и создал главную страницу

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import { Route, Routes } from "react-router-dom";
 import Footer from "./Components /Footer/Footer";
 import Toolbar from "./Components /Toolbar/Toolbar";
+import Home from "./Containers/Home";
 
 const App = () => {
   return (
@@ -7,9 +9,22 @@ const App = () => {
       <header>
         <Toolbar />
       </header>
-      <main className="container"></main>
+      <main className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Routes>
+          <Route path="/fighters" element={<Home />} />
+        </Routes>
+        <Routes>
+          <Route path="/rating" element={<Home />} />
+        </Routes>
+        <Routes>
+          <Route path="/fight" element={<Home />} />
+        </Routes>
+      </main>
       <footer>
-        <Footer/>
+        <Footer />
       </footer>
     </>
   );
